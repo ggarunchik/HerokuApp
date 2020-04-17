f94b6d9ec13da4c41c0d1977295e5b17a16a6972
Gleb Garunchik <garunchikgleb@gmail.com>
2020-04-17 13:12:49 +0300

    -* Notification Messages - кликнуть на кнопку, дождаться появления нотификации, проверить соответствие текста ожиданиям

---
+++ src/test/java/tests/NotificationPageTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.NotificationPage;
+
+public class NotificationPageTest extends BaseTest {
+
+    @Test
+    public void verifySuccessAlertMessage() {
+        NotificationPage notificationPage = new NotificationPage(driver);
+        notificationPage
+                .openNotificationPage()
+                .verifySuccessAlert()
+                .verifyUnSuccessAlert();
+    }
+}
